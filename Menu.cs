69cf4f546b3e4b43bd0e69263421beca9f22ca92
gbri-dev <gabriel.silva@el.com.br>
2022-08-23 11:50:08 -0300

    add Função handleMenu

--- Menu.cs
+++ Menu.cs
@@ -10,7 +10,13 @@ namespace EditorHtml
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
+
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
+
+            HandleMenu(option);
         }
 
         public static void DrawScreen()
@@ -38,5 +44,42 @@ namespace EditorHtml
             Console.Write("+");
             Console.Write("\n");
         }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);//Coluna 3 linha 2
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("==========");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo: ");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+        }
+
+        public static void HandleMenu(short option)
+        {
+            Console.Clear();
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Novo arquivo");
+                    break;
+                case 2:
+                    Console.WriteLine("Abrir um arquivo");
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+            }
+
+        }
+
     }
 }
